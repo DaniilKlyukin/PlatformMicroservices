7e74b364e99873cb107ef7486a557a4d4f4febf9
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-06-08 11:38:41 +0400

    Create PlatformsController.cs

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data.Interfaces;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo _platformRepo;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(
+        IPlatformRepo platformRepo,
+        IMapper mapper)
+    {
+        _platformRepo = platformRepo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Получение платформ...");
+
+        var platforms = _platformRepo.GetAll();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+}
