a720a609f72df2a6a536088614742e84a59a4718
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-06-08 11:38:31 +0400

    add repo

---
+++ PlatformService/Data/Interfaces/IPlatformRepo.cs
@@ -0,0 +1,14 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data.Interfaces;
+
+public interface IPlatformRepo
+{
+    bool SaveChanges();
+
+    IEnumerable<Platform> GetAll();
+
+    Platform? GetById(int id);
+
+    void Create(Platform platform);
+}
---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Data.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PlatformRepo : IPlatformRepo
+{
+    private readonly AppDbContext _context;
+
+    public PlatformRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Create(Platform platform)
+    {
+        ArgumentNullException.ThrowIfNull(platform);
+
+        _context.Platforms.Add(platform);
+    }
+
+    public IEnumerable<Platform> GetAll()
+    {
+        return _context.Platforms.AsNoTracking().ToList();
+    }
+
+    public Platform? GetById(int id)
+    {
+        return _context.Platforms.Find(id);
+    }
+
+    public bool SaveChanges()
+    {
+        return _context.SaveChanges() >= 0;
+    }
+}
