cbf8becaccfba53eae67f38a4b16e879336750d8
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-06-08 11:38:11 +0400

    add model

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
+
+public class Platform
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Publisher { get; set; } = string.Empty;
+
+    public string Cost { get; set; } = string.Empty;
+}
