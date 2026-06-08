124301af0a69100fe304af9d488e388f425f3e02
Daniil_Klyukin <anatoliikljukin@mail.ru>
2026-06-08 11:38:43 +0400

    Update Program.cs

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,25 +1,39 @@
-namespace PlatformService
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+using PlatformService.Data.Common;
+using PlatformService.Data.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddAutoMapper(cfg => cfg.AddMaps(AppDomain.CurrentDomain.GetAssemblies()));
+
+if (builder.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    Console.WriteLine("--> Использование InMemory базы данных (Development)");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseInMemoryDatabase("InMem"));
 }
+else
+{
+    Console.WriteLine("--> Использование SQL Server (Production)");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapOpenApi();
+
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
+
+app.Run();
